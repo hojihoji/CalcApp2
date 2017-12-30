7ef840202768bd7c86001aa7623009a026bb4692
yuuya.mito <yuya.m.tokai@gmail.com>
2017-12-30 17:23:34 +0900

    inputTypeの追加（数値、小数点、負の数に制限) Try catchの追加 (NumberFormatExceptionに対応) 例外時にアラートダイアログを追加

--- app/src/main/java/jp/techacademy/mito/yuuya/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/mito/yuuya/calcapp/MainActivity.java
@@ -1,11 +1,18 @@
 package jp.techacademy.mito.yuuya.calcapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import static android.text.InputType.TYPE_NUMBER_FLAG_DECIMAL;
+import static android.text.InputType.TYPE_NUMBER_FLAG_SIGNED;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,7 +26,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         aEditText =(EditText) findViewById(R.id.editText1) ;
+        aEditText.setInputType(InputType.TYPE_CLASS_NUMBER|TYPE_NUMBER_FLAG_DECIMAL|TYPE_NUMBER_FLAG_SIGNED);
         bEditText =(EditText) findViewById(R.id.editText2) ;
+        bEditText.setInputType(InputType.TYPE_CLASS_NUMBER|TYPE_NUMBER_FLAG_DECIMAL|TYPE_NUMBER_FLAG_SIGNED);
+
 
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setText("+");
@@ -53,11 +63,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         Intent intent = new Intent(this, SecondActivity.class);
-        double num1 = Double.parseDouble(aEditText.getText().toString());
-        double num2 = Double.parseDouble(bEditText.getText().toString());
-        intent.putExtra("VALUE1", num1);
-        intent.putExtra("VALUE2", num2);
-        intent.putExtra("VALUE3", a);
-        startActivity(intent);
+        try {
+            double num1 = Double.parseDouble(aEditText.getText().toString());
+            double num2 = Double.parseDouble(bEditText.getText().toString());
+            intent.putExtra("VALUE1", num1);
+            intent.putExtra("VALUE2", num2);
+            intent.putExtra("VALUE3", a);
+            startActivity(intent);
+        }
+        //数値以外を入力された時のアラートダイアログ
+        catch(java.lang.NumberFormatException ex){
+            //AlertDialog.Builderクラスを使用してAlertDialogを準備する。
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                alertDialogBuilder.setTitle("警告");
+                alertDialogBuilder.setMessage("フォームに数値を入力してください。");
+
+                //肯定ボタンの文字列、押した時のリスナーを設定する
+                alertDialogBuilder.setPositiveButton("閉じる",
+                        new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog,int witch){
+                                Log.d("UI_PARTS","閉じるボタン");
+                            }
+                        });
+                //AlertDialogを作成して表示する
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+        }
+
     }
 }
--- app/src/main/java/jp/techacademy/mito/yuuya/calcapp/SecondActivity.java
+++ app/src/main/java/jp/techacademy/mito/yuuya/calcapp/SecondActivity.java
@@ -8,28 +8,26 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
 
     @Override
+    //TEST
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
         Intent intent = getIntent();
         double value1 = intent.getDoubleExtra("VALUE1", 0);
         double value2 = intent.getDoubleExtra("VALUE2", 0);
         String value3 = intent.getStringExtra("VALUE3");
 
         TextView textView = (TextView) findViewById(R.id.textView);
-        if(value3.equals("+")) {
-            textView.setText(String.valueOf(value1 + value2));
-        }else if(value3.equals("-")) {
-            textView.setText(String.valueOf(value1 - value2));
-        }else if(value3.equals("×")) {
-            textView.setText(String.valueOf(value1 * value2));
-        }else if(value3.equals("÷")&&value1>0&&value2>0) {
-            textView.setText(String.valueOf(value1 / value2));
-        }else {
-            textView.setText("計算できません");
-        }
-
-
+            if (value3.equals("+")) {
+                textView.setText(String.valueOf(value1 + value2));
+            } else if (value3.equals("-")) {
+                textView.setText(String.valueOf(value1 - value2));
+            } else if (value3.equals("×")) {
+                textView.setText(String.valueOf(value1 * value2));
+            } else if (value3.equals("÷") && value1 != 0 && value2 != 0) {
+                textView.setText(String.valueOf(value1 / value2));
+            } else {
+                textView.setText("計算できません");
+            }
     }
 }
